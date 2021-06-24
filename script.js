75d2825b673cc16b187807ebf3ed4b026be6a8b4
Frederikevk <84713497+Frederikevk@users.noreply.github.com>
2021-06-24 16:16:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const muurVerven = function(welkeMuur, kleur) {
+    console.log("De "  + welkeMuur + "muur is geverfd in de kleur " + kleur);
+}
+
+muurVerven("linker", "geel");
+muurVerven("rechter", "rood");
+
+
+
+
